a9a9bb4bab17c6c23b2fdfddefa2ab4722fc1ebe
Priyesh Patel <ppatel@broadleafcommerce.org>
2013-02-01 15:37:35 -0600

    change reason to data driven enum

--- core/src/main/resources/sql/load_data_oms.sql
+++ core/src/main/resources/sql/load_data_oms.sql
@@ -15,3 +15,15 @@ insert into BLC_DATA_DRVN_ENUM_VAL values (2,'PAYMENT',false,'PAYMENT',1);
 
 insert into BLC_DATA_DRVN_ENUM_VAL values (3,'UNKNOWN',false,'UNKNOWN',1);
 insert into BLC_DATA_DRVN_ENUM_VAL values (4,'OTHER',false,'OTHER',1);
+
+
+insert into BLC_DATA_DRVN_ENUM values (2,'RETURN_REASON_TYPE',true);
+
+
+insert into BLC_DATA_DRVN_ENUM_VAL values (10,'DEFECTIVE',false,'DEFECTIVE',2);
+insert into BLC_DATA_DRVN_ENUM_VAL values (11,'NEVER_RECEIVED',false,'NEVER_RECEIVED',2);
+
+insert into BLC_DATA_DRVN_ENUM_VAL values (12,'RECEIVED_WRONG_ITEM',false,'RECEIVED_WRONG_ITEM',2);
+insert into BLC_DATA_DRVN_ENUM_VAL values (13,'NOT_WHAT_I_WANTED',false,'NOT_WHAT_I_WANTED',2);
+insert into BLC_DATA_DRVN_ENUM_VAL values (14,'CHANGED_MY_MIND',false,'CHANGED_MY_MIND',2);
+insert into BLC_DATA_DRVN_ENUM_VAL values (15,'OTHER',false,'OTHER',2);
